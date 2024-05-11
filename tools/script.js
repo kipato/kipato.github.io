4cb2e95b3de5be4a85f8f0210e5a6590988076d6
k <kp@5b.ee>
2024-05-11 11:47:16 +0530

    paste, removed go button, input validation

--- tools/script.js
+++ tools/script.js
@@ -1,9 +1,27 @@
 $(document).ready(function () {
-  $("#button").click(function (e) {
-    var inputvalue = $("#input").val();
-    // window.open('http://wa.me/91' + inputvalue);
-    $("#wa-link").attr("href", "https://wa.me/91" + inputvalue);
-    $(".number").html("+91 " + inputvalue);
-    $("#tc-link").attr("href", "https://www.truecaller.com/search/in/" + inputvalue);
+  $("#input").on("input", function () {
+    validateInput($(this).val());
   });
+
+  $("#paste-btn").on("click", function () {
+    navigator.clipboard.readText().then(function (clipText) {
+      $("#input").val(clipText);
+      validateInput(clipText);
+    });
+  });
+
+  function validateInput(inputvalue) {
+    var cleanedValue = inputvalue.replace(/\D/g, '');
+    var lastTenDigits = cleanedValue.slice(-10);
+
+    if (lastTenDigits.length === 10) {
+      $("#wa-link").attr("href", "https://wa.me/91" + lastTenDigits).removeClass("disabled");
+      $(".number").html("+91 " + lastTenDigits);
+      $("#tc-link").attr("href", "https://www.truecaller.com/search/in/" + lastTenDigits).removeClass("disabled");
+      $(".logo-btn").removeClass("desaturate");
+    } else {
+      $("#wa-link, #tc-link").addClass("disabled").removeAttr("href");
+      $(".logo-btn").addClass("desaturate");
+    }
+  }
 });
